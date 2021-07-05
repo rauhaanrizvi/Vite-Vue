f94b51b772615d83ba1be729bd11bd7f933639e6
Lau Tiam kok <lau.tiamkok@yahoo.co.uk>
2021-07-05 03:07:01 +0200

    Updated code

--- Advanced/src/store/actions.js
+++ Advanced/src/store/actions.js
@@ -6,7 +6,26 @@ const apiBaseUrl = import.meta.env.VITE_REMOTE_API_BASE_URL
 
 export default {
   async fetchSiteInfo ({ state, commit, rootState }) {
-    const { data } = await axios.get(apiBaseUrl + 'site')
-    commit('setSiteInfo', data)
+
+    // Get the data from the local storage.
+    // Web Storage (localStorage and sessionStorage)
+    let siteInfo = localStorage.getItem('site-info')
+
+    // If the item exists, then parse it.
+    if (siteInfo !== null) {
+      siteInfo = JSON.parse(siteInfo)
+    } else {
+      const { data } = await axios.get(apiBaseUrl + 'site')
+
+      // Store the data to the localStorage.
+      // Like cookies, you only can store the data as string.
+      siteInfo = JSON.stringify(data)
+      localStorage.setItem('site-info', siteInfo)
+    }
+    // Remove localstorage data.
+    // localStorage.removeItem('site-info')
+
+    // commit('setSiteInfo', data)
+    commit('setSiteInfo', siteInfo)
   }
 }
