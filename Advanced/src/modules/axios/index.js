55f24fd57ac89515ef9fa9985f24a7ce88daa73a
Lau Tiam kok <lau.tiamkok@yahoo.co.uk>
2021-07-07 14:25:06 +0200

    Updated code

--- Advanced/src/modules/axios/index.js
+++ Advanced/src/modules/axios/index.js
@@ -4,7 +4,4 @@ import axios from 'axios'
 
 export default axios.create({
   baseURL: import.meta.env.VITE_REMOTE_API_BASE_URL
-
-  // Or, using options to set the remote API base URL.
-  // baseURL: options.baseUrl
 })
