2d245c87824ff3211bc23c9be5d8b13bb27cf09c
Lau Tiam kok <lau.tiamkok@yahoo.co.uk>
2021-07-08 17:29:54 +0200

    Updated code

---
+++ Advanced/puppeteer.js
@@ -0,0 +1,27 @@
+// Basic example of crawling a SPA with Puppeteer.
+// Usage:
+// $ node puppeteer.js
+// https://github.com/puppeteer/puppeteer
+// https://andrejsabrickis.medium.com/scrapping-the-content-of-single-page-application-spa-with-headless-chrome-and-puppeteer-d040025f752b
+const puppeteer = require('puppeteer');
+
+(async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:5000/about/');
+    await page.waitForSelector('#app', { timeout: 1000 });
+
+    // const body = await page.evaluate(() => {
+    //   return document.querySelector('body').innerHTML;
+    // });
+    // console.log(body);
+
+    const html = await page.content();
+    console.log(html);
+
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+  }
+})();
