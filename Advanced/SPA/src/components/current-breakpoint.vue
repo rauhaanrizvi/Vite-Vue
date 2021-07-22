6a8d680eea455dbb03d2d6d14acfd33b89fc1850
Lau Tiam kok <lau.tiamkok@yahoo.co.uk>
2021-07-22 10:35:23 +0200

    Updated code

--- Advanced/SPA/src/components/current-breakpoint.vue
+++ Advanced/SPA/src/components/current-breakpoint.vue
@@ -4,11 +4,11 @@
     <!-- https://gist.github.com/jonsugar/6bce22bd7d3673294caad36046c2b7cb !-->
     <div class="fixed bottom-0 right-0 py-1.5 px-3 bg-black text-white text-sm">
       <span>Current breakpoint: </span>
-      <span class="sm:hidden md:hidden lg:hidden xl:hidden">&lt; sm</span>
-      <span class="hidden sm:inline md:hidden font-extrabold">sm</span>
-      <span class="hidden md:inline lg:hidden font-extrabold">md</span>
-      <span class="hidden lg:inline xl:hidden font-extrabold">lg</span>
-      <span class="hidden xl:inline font-extrabold">xl</span>
+      <span class="sm:hidden md:hidden lg:hidden xl:hidden font-bold">&lt; sm</span>
+      <span class="hidden sm:inline md:hidden font-bold">sm</span>
+      <span class="hidden md:inline lg:hidden font-bold">md</span>
+      <span class="hidden lg:inline xl:hidden font-bold">lg</span>
+      <span class="hidden xl:inline font-bold">xl</span>
     </div>
 
   </div>
